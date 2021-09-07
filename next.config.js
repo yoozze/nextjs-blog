332a3b59b1ffc8d95228c01a651bab3d965749b4
yoozze <ezzooy@gmail.com>
2021-09-07 09:59:23 +0200

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
     reactStrictMode: true,
-    assetPrefix: '.'
+    basePath: isProd ? '/nextjs-blog' : '',
+    assetPrefix: isProd ? '/nextjs-blog/' : '',
 };
